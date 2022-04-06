f6ba708768c1e56e8f56f2c17ada27719a4ee81e
ccampbell04 <cjscampbell@icloud.com>
2022-04-06 19:36:37 +0100

    Chapter 9 - 1 test failing, hopefully next commit fixes

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -135,3 +135,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login
+
+LOGIN_URL = 'rango:login'
